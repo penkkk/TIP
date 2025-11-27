e5caf456fe6b12e07bc358f76da346d2bbff50db
Petrov Artem <artem_petrov_0407@mail.ru>
2025-11-27 20:58:23 +0300

    kr-4

--- KR_4/1.cpp
+++ KR_4/1.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int main()
 {
     int n;
+    cout << "Введите количество задач: ";
     cin >> n;
-    vector<int> squad;
-    for (int i = 1; i * i <= n; i++)
+    vector<int> time(n + 1);
+    vector<vector<int>> addiction(n + 1);
+    vector<int> finish(n + 1, 0);
+    cout << endl;
+
+    for (int i = 1; i < n + 1; i ++)
     {
-        squad.push_back(i*i);
-    }
-    vector<int> result(n + 1);
-    result[0] = 1;
-    for (int i: squad)
-    {
-        for (int j = i; j <= n; j ++)
-        {
-            result[j] += result[j - i];
+        cout << "Введите время " << i << " задачи и количество ее зависимостей, а затем преречислите их: ";
+        int k;
+        cin >> time[i] >> k;
+        for (int j = 0; j < k; j ++)
+        {   
+            int a;
+            cin >> a;
+            addiction[i].push_back(a);
         }
     }
-    for (int i = 0; i < result.size(); i ++)
+    for (int i = 1; i < n + 1; i ++)
     {
-        cout << result[i] << ' ';
+        int high = 0;
+        for (auto j: addiction[i])
+        {
+            high = max(high, finish[j]);
+        }
+        finish[i] = time[i] + high;
     }
-    cout << endl;
-    cout << result[n];
+
+    int res = 0;
+    for (int i = 1; i < n + 1; i ++)
+    {
+        res = max(res, finish[i]);
+    }
+    cout << res;
     return 0;
 }
---
+++ KR_4/2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> squad;
+    for (int i = 1; i * i <= n; i++)
+    {
+        squad.push_back(i*i);
+    }
+    vector<int> result(n + 1);
+    result[0] = 1;
+    for (int i: squad)
+    {
+        for (int j = i; j <= n; j ++)
+        {
+            result[j] += result[j - i];
+        }
+    }
+    for (int i = 0; i < result.size(); i ++)
+    {
+        cout << result[i] << ' ';
+    }
+    cout << endl;
+    cout << result[n];
+    return 0;
+}
