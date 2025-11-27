5f277267d90ae65e1ad0440017966130f93337d6
Petrov Artem <artem_petrov_0407@mail.ru>
2025-11-27 23:58:33 +0300

    kr4 last ver

---
+++ KR_4/3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> array = {1, 2, 4, 3, 6, 8, 5,2};
+    int arraySize = array.size();
+    vector<int> sumEven(arraySize, 0);
+    for (int i = 0; i < arraySize; i ++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            if (i == 0)
+            {
+                sumEven[i] = array[i];
+            }
+            else
+            {
+                sumEven[i] = max(array[i], array[i] + sumEven[i - 1]);
+            }
+        }
+        else 
+        {
+            sumEven[i] = 0;
+        }
+    }
+    int mx = 0;
+    for (int i = 0; i < arraySize; i ++)
+    {
+        mx = max(mx, sumEven[i]);
+    }
+    cout << mx;
+    return 0;
+}           
